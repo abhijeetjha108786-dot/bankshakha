353d63ea491a385ecf222ce60c4f10956a546ec9
abhijeetjha108786-dot <abhijeetjha108786-dot@users.noreply.github.com>
2026-04-13 13:45:45 +0530

    Fail fast MongoDB connect to prevent Vercel function timeout

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,13 @@ const mongoose = require("mongoose");
 
 async function connectDB(uri) {
   mongoose.set("strictQuery", true);
-  await mongoose.connect(uri);
+  await mongoose.connect(uri, {
+    // Fail fast in serverless to avoid Vercel timeout while selecting server.
+    serverSelectionTimeoutMS: 8000,
+    connectTimeoutMS: 10000,
+    socketTimeoutMS: 20000,
+    maxPoolSize: 5,
+  });
 }
 
 module.exports = connectDB;
